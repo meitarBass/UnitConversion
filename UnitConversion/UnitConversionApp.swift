e2e69422503e0a26ee4a5c337cdb63ffc175b2d3
meitarbasson <meitarbass@gmail.com>
2022-06-28 14:38:17 +0300

    Initial Commit

---
+++ UnitConversion/UnitConversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConversionApp.swift
+//  UnitConversion
+//
+//  Created by Meitar Basson on 28/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct UnitConversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
